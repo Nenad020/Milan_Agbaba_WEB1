dd0d81b3a44e143402381dea7ba816ea7ebb481f
Nenad020 <nenadzelenovic@gmail.com>
2021-08-29 10:52:27 +0200

    Profil korisnika i odjavljivanje

--- MVC/MVC/Controllers/LogInRegisterController.cs
+++ MVC/MVC/Controllers/LogInRegisterController.cs
@@ -9,11 +9,12 @@ namespace MVC.Controllers
 {
     public class LogInRegisterController : Controller
     {
-		//Lista korisnika
 		private List<User> users = new List<User>();
+		private List<Comment> comments = new List<Comment>();
+		private List<Reservation> reservations = new List<Reservation>();
 
-        //Otvara se prozor za registraciju korisnika
-        public ActionResult OpenRegisterPage()
+		//Otvara se prozor za registraciju korisnika
+		public ActionResult OpenRegisterPage()
         {
             return View("Register");
         }
@@ -56,6 +57,7 @@ namespace MVC.Controllers
 			return View("LogIn");
 		}
 
+		//Akcija za logovanje korisnika
 		public ActionResult LogIn(User user)
 		{
 			//Vrsi se validacija unetih vrednosti
@@ -84,9 +86,55 @@ namespace MVC.Controllers
 			return RedirectToAction("../Home/Index");
 		}
 
+		//Akcija koja otvara prozor za azuriranje profila
 		public ActionResult OpenProfilePage()
 		{
 			return View("Profile");
+		}
+
+		//Akcija koja vrsi azuriranje profila
+		public ActionResult ChangeProfile(string OldUsername, User user)
+		{
+			//Vrsi se validacija unetih vrednosti
+			bool validate = user.Validate();
+			if (validate == false)
+			{
+				ViewBag.Message = "Input fields can't be empty!";
+				return View("Profile");
+			}
+
+			//Ocitavamo sve korisnike iz baze
+			LoadUsers();
+
+			//Proveravamo da li je korisnik vec postoji u bazi sa datim kredencijalima
+			User exists = ReturnUser(OldUsername);
+			if (exists != null)
+			{
+				//Azuriramo podatke
+				exists.Username = user.Username;
+				exists.Password = user.Password;
+				exists.Name = user.Name;
+				exists.Lastname = user.Lastname;
+				exists.Gender = user.Gender;
+				exists.DateOfBirth = user.DateOfBirth;
+				exists.Email = user.Email;
+			}
+
+			//Ubacujemo korisnika u sesiju
+			System.Web.HttpContext.Current.Application["user"] = exists;
+
+			//I otvaramo akciju za ispis aktivnih aranzmana na pocetnoj stranici
+			return RedirectToAction("../Home/Index");
+		}
+
+		//Akcija za odjavljivanje korisnika
+		public ActionResult LogOff()
+		{
+			//Korisnik je null
+			System.Web.HttpContext.Current.Application["user"] = null;          
+			
+			//I otvaramo akciju za ispis aktivnih aranzmana na pocetnoj stranici
+			return RedirectToAction("../Home/Index");
 		}
 
 		//Vrsi se provera da li korisnik postoji u listi korisnika
@@ -126,6 +174,24 @@ namespace MVC.Controllers
 			return null;
 		}
 
+		//Vrsi se provera da li korisnik postoji u listi korisnika
+		private User ReturnUser(string username)
+		{
+			//Prolazimo kroz svakog korisnika
+			foreach (var user in users)
+			{
+				//Ako je korisnicko ime jednako onom sto smo prosledili kao parametar
+				if (user.Username == username)
+				{
+					//Znaci da korisnik postoji
+					return user;
+				}
+			}
+
+			//U suprotnom ne postoji
+			return null;
+		}
+
 		//Ucitavamo podatke iz baze i upisujemo u listu
 		private void LoadUsers()
 		{
@@ -136,6 +202,61 @@ namespace MVC.Controllers
 		private void SaveUsers()
 		{
 			XMLHelper.SaveUsers(users);
+		}
+
+		//Ucitavamo podatke iz baze i upisujemo u listu
+		private void LoadComments()
+		{
+			comments = XMLHelper.LoadComments();
+		}
+
+		//Listu korisnika upisujemo u bazu
+		private void SaveComments()
+		{
+			XMLHelper.SaveComments(comments);
+		}
+
+		//Ucitavamo podatke iz baze i upisujemo u listu
+		private void LoadReservations()
+		{
+			reservations = XMLHelper.LoadReservations();
+		}
+
+		//Listu korisnika upisujemo u bazu
+		private void SaveReservations()
+		{
+			XMLHelper.SaveReservations(reservations);
+		}
+
+		//Ova metoda se poziva kada turista promeni svoje korisnicno ime
+		private void UpdateTouristUsername(User user, string oldUsername)
+		{
+			//Ucitavaju se komentari i rezervatije
+			LoadComments();
+			LoadReservations();
+
+			foreach (var comment in comments)
+			{
+				//Ako turista koji je ostavio komentar odgovara sa turistom koji menja profil i koji je promeni korisnicko ime
+				if (comment.TouristUsername == oldUsername)
+				{
+					//Samo u komentaru zameni mu korisnicko ime sa novim
+					comment.TouristUsername = user.Username;
+				}
+			}
+
+			//Ista logika i ovde
+			foreach (var reservation in reservations)
+			{
+				if (reservation.TouristUsername == oldUsername)
+				{
+					reservation.TouristUsername = user.Username;
+				}
+			}
+
+			//Sacuvamo izmene
+			SaveComments();
+			SaveReservations();
 		}
 	}
 }
